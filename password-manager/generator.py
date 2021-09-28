615c8fda2a344c194db7b5aacd4f6a7b69ff3fd6
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2021-09-28 19:06:28 +0300

    Added password encryption

--- password-manager/generator.py
+++ password-manager/generator.py
@@ -1,11 +1,14 @@
 import configparser
 from os import close
+from cryptography import fernet
+from cryptography.fernet import Fernet
 import random
 
 config = configparser.ConfigParser()
 config.read('password-manager/config.ini')
 
 user_password = config.get('user', 'user_password')
+key = config.get('user', 'user_key')
 
 uppder_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lower_case = 'abcdefghijklmnopqrdtuvwxyz'
@@ -29,6 +32,18 @@ while not user_authenticated:
         remaining_tries -= 1
         print('Incorrect password! Remaining tries:', remaining_tries)
 
+def encrypt_password(password):
+    encoded_password = password.encode()
+    f = Fernet(key)
+    encrypted_password = f.encrypt(encoded_password)
+    print(encrypted_password.decode())
+    return encrypted_password
+
+def decrypt_password(encrypted_password):
+    f = Fernet(key)
+    decrypted_password = f.decrypt(encrypted_password)
+    print(decrypted_password.decode())
+
 length = int(input('Enter your desired password length: '))
 
 for i in range(length):
@@ -36,3 +51,5 @@ for i in range(length):
 
 #print(user_password)
 print(password)
+encrypted_password = encrypt_password(password)
+decrypt_password(encrypted_password)
