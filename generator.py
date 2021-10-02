6b5c904d0157008e1de61ec45728d931af1b52cc
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2021-10-02 18:35:14 +0300

    Added database migration

--- generator.py
+++ generator.py
@@ -1,11 +1,10 @@
 import configparser
-from os import close
 from cryptography import fernet
 from cryptography.fernet import Fernet
 import random
 
 config = configparser.ConfigParser()
-config.read('password-manager/config.ini')
+config.read('config.ini')
 
 user_password = config.get('user', 'user_password')
 key = config.get('user', 'user_key')
@@ -14,7 +13,6 @@ uppder_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lower_case = 'abcdefghijklmnopqrdtuvwxyz'
 numbers = '1234567890'
 symbols = '.,!@#$%^&*()'
-password = ''
 remaining_tries = 3
 
 combined_string = uppder_case + lower_case + numbers + symbols
@@ -23,7 +21,7 @@ user_authenticated = False
 while not user_authenticated:
     if remaining_tries == 0:
         print('You typed incorrect password 3 times in a row, access is denied!')
-        close()
+        exit()
     auth_password = input('Please enter your password: ')
     if auth_password == user_password:
         print('welcome back!')
@@ -44,12 +42,21 @@ def decrypt_password(encrypted_password):
     decrypted_password = f.decrypt(encrypted_password)
     print(decrypted_password.decode())
 
-length = int(input('Enter your desired password length: '))
+def generate_password(length):
+    password = ''
+    if length > 32:
+        print('The maximum length of password can be 32 characters!')
+        return
+    for i in range(length):
+        password += random.choice(combined_string)
+    encrypted_password = encrypt_password(password)
+    decrypt_password(encrypted_password)
+    print(password)
+    return password
 
-for i in range(length):
-    password += random.choice(combined_string)
+try:
+    length = int(input('Enter your desired password length: '))
+    generate_password(length)
+except:
+    print('Only numbers are allowed!')
 
-#print(user_password)
-print(password)
-encrypted_password = encrypt_password(password)
-decrypt_password(encrypted_password)
--- password-manager/migration.py
+++
@@ -1,7 +0,0 @@
-import logging
-import logging.config
-import mysql.connector
-import yaml
-import os
-import time
-from datetime import datetime
