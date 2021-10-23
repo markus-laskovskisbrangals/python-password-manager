7606dc2766088a3271b194abb439f058c024a22b
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2021-10-23 21:41:44 +0300

    Added unit tests

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 from logging import error
 from generator import *
 
-if __name__ == '__main__':
-    assert encrypt_password(12)
-    assert encrypt_password(0)
-    assert encrypt_password(-3)
-    assert encrypt_password('a')
-    assert encrypt_password([1]) == error, 'Should be error'
+print('testēju paroles šifrēšanu ar string')
+assert encrypt_password('sdqeasdq.rae13')
+print('testēju paroles šifrēšanas atgriezto datu tipu (jābūt bytearray')
+assert type(encrypt_password('sdqeasdq.rae13')) == bytes
+print('testēju paroles šifrēšanu ar skaitli')
+assert encrypt_password(12) == 'bad'
+
+print('Testēju paroles atšifrēšanu.')
+assert decrypt_password('gAAAAABhWKiDIw0qABLW2E5bP-egsfNkb3JOfxAhb3gn-gEfHua116wQrH9unbdk7E477FGY6RBh-cgBp3LSEzhg51XATFsxpA=='.encode()) == 'wYtxdg.3RLoB'
+print('Testēju paroles atšifrēšanu ar skaitli')
+assert decrypt_password(1234) == 'bad'
